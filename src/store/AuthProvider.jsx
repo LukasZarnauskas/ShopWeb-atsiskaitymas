ef88538a00e568646c7befd1423720c25e51d932
GluteusMedius <zarnauskas97@gmail.com>
2023-04-20 19:27:05 +0300

    AuthProvider

---
+++ src/store/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext({
+    user: {},
+    isLoading: false,
+    login() {},
+    logout() {},
+    register() {},
+})
+
+
+
+
+function AuthProvider({ children }) {
+const [user, setUser] = useState({})
+const [isLoading, setIsLoading] = useState(false)
+
+
+const authCtx = {
+user,
+isLoading,
+}
+
+
+  return (
+    <AuthContext.Provider value={authCtx} >{children}</AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
+
+export function useAuthCtx(){
+return useContext(AuthContext)
+}
