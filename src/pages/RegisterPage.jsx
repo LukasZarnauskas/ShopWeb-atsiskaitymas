e212091cac9c5deed943ac271e0f149265e45d66
GluteusMedius <zarnauskas97@gmail.com>
2023-04-21 13:59:06 +0300

    Register work

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import RegisterForm from '../auth/RegisterForm'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase/firebase';
 
 function RegisterPage() {
+
+  function userRegisterFire({email, password}){
+
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  }
   return (
     <div>
-
-      <RegisterForm/>
+<h1>Register</h1>
+      <RegisterForm onRegister={userRegisterFire}/>
     </div>
      )
 }
