b461cd61c81e996550c422c55df1b3d7e078a7c4
GluteusMedius <zarnauskas97@gmail.com>
2023-04-21 18:20:08 +0300

    auth folder move to components

--- src/components/auth/Logout.jsx
+++ src/components/auth/Logout.jsx
@@ -1,7 +1,7 @@
 import { signOut } from 'firebase/auth';
 import React from 'react'
-import { auth } from '../firebase/firebase';
-import { useAuthCtx } from '../store/AuthProvider';
+import { auth } from '../../firebase/firebase';
+import { useAuthCtx } from '../../store/AuthProvider';
 
 function Logout() {
  
