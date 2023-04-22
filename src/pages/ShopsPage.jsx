ea073da213ea622b7e74f116f019e52234200d49
GluteusMedius <zarnauskas97@gmail.com>
2023-04-22 13:48:16 +0300

    getShops  in shops page, get data from firebase

--- src/pages/ShopsPage.jsx
+++ src/pages/ShopsPage.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { collection, getDocs } from "firebase/firestore"; 
+import { db } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+
+
 
 function ShopsPage() {
+  
+const [shopsArr, setShopsArr] = useState([])
+
+useEffect(() => {
+  async function getShop(){
+
+const querySnapshot = await getDocs(collection(db, "shops"));
+const tempShops = [];
+
+querySnapshot.forEach((doc) => {
+  // console.log(`${doc.id} => ${doc.data()}`);
+ tempShops.push({
+  uid: doc.id,
+  ...doc.data(),
+ })
+});
+setShopsArr(tempShops)
+  }
+  getShop()
+}, [])
+console.log(shopsArr);
+
   return (
-    <div>ShopsPage</div>
+    <div>
+      <h1>Shops page</h1>
+      <ul>
+        {shopsArr.map((sObj) => <li key={sObj.uid}>{sObj.name}</li>) }
+      </ul>
+    </div>
   )
 }
 
