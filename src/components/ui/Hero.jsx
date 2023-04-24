a1a690579e0a6b9d1c72aa5dc61f36851dbd0959
GluteusMedius <zarnauskas97@gmail.com>
2023-04-25 00:45:40 +0300

    hero background

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className="flex flex-col justify-center items-center mb-10">
-    <h1 className='text-4xl font-bold'>Shops Web</h1>
-    <p className='text-xl text-gray-500'>All shops in one place</p>
+    <div className="bg-gradient-to-r w-full h-72 from-cyan-100 to-teal-500  flex flex-col justify-center items-center mb-10">
+    <h1 className='text-4xl font-bold mb-3'>Shops Web</h1>
+    <p className='text-xl text-gray-600 font-semibold'>All shops in one place</p>
     </div>
   )
 }
